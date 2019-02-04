57bcf9e6eb0b7573d5df2424648692da041ca516
maty21 <maty21@gmail.com>
2019-02-04 10:54:51 +0200

    fix dependencies

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const Octokit = require('@octokit/rest');
 const axios = require('axios');
 const { request, GraphQLClient } = require('graphql-request');
 //https://api.github.com/graphql
-const token ='052645882dea5cdb43d767e254e20cc591679e20'
-const query =async (atoken)=>{
-    const { data } = await graphql(`{ repository(owner:"maty21" name:"test"){
+const token = '052645882dea5cdb43d767e254e20cc591679e20'
+const query = async (atoken) => {
+  const { data } = await graphql(`{ repository(owner:"maty21" name:"test"){
         issues(last:100){
         edges{
           node{
@@ -26,20 +26,22 @@ const query =async (atoken)=>{
       }  
       
     }
-    }`,{
-        headers: {
-          authorization: `token ${atoken}`
-        }
+    }`, {
+      headers: {
+        authorization: `token ${atoken}`
+      }
     })
-    return data
+  return data
 }
-const octokit = new Octokit()
-
-octokit.authenticate({
-  type: 'oauth',
-  token
+const octokit = new Octokit({
+  auth: token
 })
-const commentMutation =async (id,version,result) =>{
+
+// octokit.authenticate({
+//   type: 'oauth',
+//   token
+// })
+const commentMutation = async (id, version, result) => {
   const { data } = await graphql(`mutation {
     addComment(input:{clientMutationId: "2", subjectId:"${id}" , body: "#### version:\n ${version}\n  #### result:\n  ${result}"\n}) {
       clientMutationId
@@ -57,95 +59,95 @@ const commentMutation =async (id,version,result) =>{
         }
       }
     }
-  }`,{
-    headers: {
-      authorization: `token ${token}`
-    }
-})
-return data
+  }`, {
+      headers: {
+        authorization: `token ${token}`
+      }
+    })
+  return data
 }
 
 //f8fed92a11c7b0bee9a4de7a87d167b04e3eff81
 
 
-const getObjectFromData = (query)=>{
-    const {edges}=  query.repository.issues;
-    const res = edges.map(issue=>{
-        const {title,bodyText,number,id} = issue.node;
-        const clearBodyText= bodyText.replace(/\n/g, '');
-        const description = clearBodyText.split('Description')[1].split('Expected Results')[0];
-        const expectedResults = clearBodyText.split('Description')[1].split('Expected Results')[1];
-        return {
-           subjectId:id,
-            id:number,
-            title,
-            description,
-            expectedResults
-        }
-    })
-    return res;
-    
+const getObjectFromData = (query) => {
+  const { edges } = query.repository.issues;
+  const res = edges.map(issue => {
+    const { title, bodyText, number, id } = issue.node;
+    const clearBodyText = bodyText.replace(/\n/g, '');
+    const description = clearBodyText.split('Description')[1].split('Expected Results')[0];
+    const expectedResults = clearBodyText.split('Description')[1].split('Expected Results')[1];
+    return {
+      subjectId: id,
+      id: number,
+      title,
+      description,
+      expectedResults
+    }
+  })
+  return res;
+
   //  query.repository.issues.edges[0].node.bodyText.split('Description')[1].split('Expected Results')[0]
-  
+
 }
 
-const queryApi = async(token) =>{
+const queryApi = async (token) => {
   try {
     const data = await query(token);
     const res = getObjectFromData(data);
     console.log(res)
-    return res;   
+    return res;
   } catch (error) {
     console.log(error);
-    
+
   }
- 
+
 }
-const addLabelToIssue = async(issueId,version,label) => {
+const addLabelToIssue = async (issueId, version, label) => {
   try {
-    
+
     //const result = await octokit.issues.createLabel({owner:"maty21", repo:"test", name:"testHub", color:"de3de3", description:""})
-    const result2 = await octokit.issues.addLabels({owner:"maty21", repo:"test",  number:1, labels:["testHub"]})
+    const result2 = await octokit.issues.addLabels({ owner: "maty21", repo: "test", number: 1, labels: ["testHub"] })
   }
-   catch (error) {
+  catch (error) {
     console.log(error);
-      
+
   }
 }
 
-commentAndLabel = async(version,id,result)=>{
-  commentMutation(id,version,result)
+commentAndLabel = async (version, id, result) => {
+  commentMutation(id, version, result)
 
 }
 
-const getUserDetails = async(token)=> {
+const getUserDetails = async (token) => {
   try {
-      let obj = new Octokit();
-      obj.authenticate({
-          type: 'app',
-          token
+    let obj = new Octokit();
+    obj.authenticate({
+      type: 'app',
+      token
+    })
+    //   const repos = await obj.repos.getAll();
+    const userDetails = await axios.get(`https://api.github.com/user?access_token=${token}`)
+    const { name, login, avatar_url, organizations_url, repos_url } = userDetails.data;
+    const repos = await axios.get(repos_url);
+    //     console.log(repos);
+    //  const specificRepo = await obj.repos.get({ owner: repos.data[0].owner.login, repo: repos.data[0].name });
+    //    console.log(specificRepo);
+    return (
+      {
+        name, login, avatar_url
+        // repos: repos.data.map(r => r.full_name)
       })
-   //   const repos = await obj.repos.getAll();
-      const userDetails = await axios.get(`https://api.github.com/user?access_token=${token}`)
-      const { name, login, avatar_url,organizations_url,repos_url } = userDetails.data;
-      const repos = await axios.get(repos_url);
-      //     console.log(repos);
-      //  const specificRepo = await obj.repos.get({ owner: repos.data[0].owner.login, repo: repos.data[0].name });
-      //    console.log(specificRepo);
-      return (
-          {
-             name, login, avatar_url 
-             // repos: repos.data.map(r => r.full_name)
-          })
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 }
 
-      module.exports = {
-        query :queryApi,
-        commentMutation:commentMutation,
-        addLabelToIssue,
-        commentAndLabel,
-        getUserDetails
-      }
+module.exports = {
+  query: queryApi,
+  commentMutation: commentMutation,
+  addLabelToIssue,
+  commentAndLabel,
+  getUserDetails
+}
